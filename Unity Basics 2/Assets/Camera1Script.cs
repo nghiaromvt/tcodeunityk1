cd21f0fed117d9cede010915077c84cf095c1703
nghiaromvt <nghiaromvt@gmail.com>
2018-12-26 17:16:37 +0700

    Unity Basics 2

---
+++ Unity Basics 2/Assets/Camera1Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera1Script : MonoBehaviour {
+    public Rigidbody2D sprite1, sprite2, sprite3, sprite4;
+
+	void FixedUpdate () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            sprite1.AddForce(Vector2.up * 500);
+        }
+        sprite2.velocity = new Vector2(5, 0);
+        sprite3.AddForce(Vector2.left * 5,ForceMode2D.Force);
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            sprite4.AddForce(Vector2.left * 15, ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            sprite4.AddForce(Vector2.right * 15, ForceMode2D.Force);
+        }
+
+    }
+}
---
+++ Unity Basics 2/Assets/Camera2Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2Script : MonoBehaviour
+{
+    public Rigidbody object1, object2, object3, object4;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            object1.AddForce(Vector2.up * 500);
+        }
+        object2.velocity = new Vector2(5, 0);
+        object3.AddForce(Vector2.left * 5, ForceMode.Force);
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            object4.AddForce(Vector2.left * 15, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            object4.AddForce(Vector2.right * 15, ForceMode.Force);
+        }
+    }
+}
