dc40a0e21da587044eb5c5f187e28e50328daf26
nghiaromvt <nghiaromvt@gmail.com>
2019-01-03 16:28:04 +0700

    DinoGame

---
+++ GameDino/Assets/Scripts/DinoScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DinoScript : MonoBehaviour {
+    public float JumpForce=300f;
+    private Rigidbody2D myBody;
+    public Text gameOver; 
+	// Use this for initialization
+	void Start () {
+        myBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            myBody.AddForce(new Vector2(0,JumpForce),ForceMode2D.Force);
+        }
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Tree"))
+        {
+            Time.timeScale = 0;
+            gameOver.gameObject.SetActive(true);
+        }
+    }
+    public void Replay()
+    {
+        GameObject[] trees;
+        trees=GameObject.FindGameObjectsWithTag("Tree");
+        foreach (GameObject tree in trees)
+        {
+            Destroy(tree.gameObject);
+        }
+        Time.timeScale = 1;
+        gameOver.gameObject.SetActive(false);
+    }
+}
+
---
+++ GameDino/Assets/Scripts/ScrollBG.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBG : MonoBehaviour {
+    public float Speed = 0.2f;
+    private MeshRenderer render;
+    private Vector2 offsetDefault;
+	// Use this for initialization
+	void Start () {
+        render = GetComponent<MeshRenderer>();
+        offsetDefault = render.sharedMaterial.GetTextureOffset("_MainTex");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+            float x = Mathf.Repeat(Time.time * Speed, 1);
+            Vector2 newOffset = new Vector2(x, offsetDefault.y);
+            render.sharedMaterial.SetTextureOffset("_MainTex", newOffset);
+    }
+}
